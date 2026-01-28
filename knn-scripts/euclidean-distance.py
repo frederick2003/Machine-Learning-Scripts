9b5358044df27e0fafcfca6cc0dcb85d7bd0cfa6
frederick2003 <fredhamilton2003@gmail.com>
2026-01-28 23:55:12 +0000

    Allowed user input for min-max scaling script

--- knn-scripts/euclidean-distance.py
+++ knn-scripts/euclidean-distance.py
@@ -10,11 +10,8 @@ def euclidean_distance(x1, x2):
     return math.sqrt(sum_squared_distance)
 
 
-x1 = [0.303, 0.870, 0.183, 0.250]
-x2 = [0.673, 0.430, 0.800, 0.900]
-q =  [0.317, 0.740, 0.267, 0.267]
-ed_x1_q = euclidean_distance(x1,q)
-ed_x2_q = euclidean_distance(x2,q)
+x1 = [0.05, 0.646]
+x2 = [0.4, 0.364]
+ed_x1_x2 = euclidean_distance(x1,x2)
 
-print(f"Euclidean distance between x1 and q: {ed_x1_q:.3f}")
-print(f"Euclidean distance between x2 and q: {ed_x2_q:.3f}")
+print(f"Euclidean distance between x1 and x2: {ed_x1_x2:.3f}")
--- knn-scripts/min-max-scaling.py
+++ knn-scripts/min-max-scaling.py
@@ -1,45 +1,8 @@
 def calculate_min_max_scaled(value, min, max):
     return (value - min) / (max - min)
 
-# Calculate the Bill Length Normalisation
-x1_bill_length = calculate_min_max_scaled(39.1, 30, 60)
-print("x1 bill length " + str(x1_bill_length))
+min = int(input("Enter min value: "))
+max = int(input("Enter max value: "))
+value = int(input("Enter feature value: "))
 
-x2_bill_length = calculate_min_max_scaled(50.2, 30, 60)
-print("x2 bill length " + str(x2_bill_length))
-
-q_bill_length = calculate_min_max_scaled(39.5, 30, 60)
-print("q bill length " + str(q_bill_length))
-print()
-
-# Calculate the Bill Depth Normalisation
-x1_bill_depth = calculate_min_max_scaled(18.7, 10, 20)
-print("x1 bill depth " + str(x1_bill_depth))
-
-x2_bill_depth = calculate_min_max_scaled(14.3, 10, 20)
-print("x2 bill depth " + str(x2_bill_depth))
-
-q_bill_depth = calculate_min_max_scaled(17.4, 10, 20)
-print("q bill depth " + str(q_bill_depth))
-print()
-
-# Calculate the Flipper Length Normalisation
-x1_flipper_length = calculate_min_max_scaled(181, 170, 230)
-print("x1 flipper length " + str(x1_flipper_length))
-
-x2_flipper_length = calculate_min_max_scaled(218, 170, 230)
-print("x2 flipper length " + str(x2_flipper_length))
-
-q_flipper_length = calculate_min_max_scaled(186, 170, 230)
-print("q flipper length " + str(q_flipper_length))
-print()
-
-# Calculate the Body Mass Normalisation
-x1_body_mass = calculate_min_max_scaled(3750, 3000, 6000)
-print("x1 body mass " + str(x1_body_mass))
-
-x2_body_mass = calculate_min_max_scaled(5700, 3000, 6000)
-print("x2 body mass " + str(x2_body_mass))
-
-q_body_mass = calculate_min_max_scaled(3800, 3000, 6000)
-print("q body mass " + str(q_body_mass))
+print(f"Min-Max Scaled value = {calculate_min_max_scaled(value, min, max):.3f}")
