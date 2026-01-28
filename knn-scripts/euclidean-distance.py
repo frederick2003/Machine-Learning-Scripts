f05b4dc9eb2915c74d9c432940612aaba8e49dd3
frederick2003 <fredhamilton2003@gmail.com>
2026-01-28 01:34:58 +0000

    First commit with Min-Max-Scaling and Euclidean Distance scripts

---
+++ knn-scripts/euclidean-distance.py
@@ -0,0 +1,15 @@
+import math
+
+def euclidean_distance(x1, x2):
+    if len(x1) != len(x2):
+        raise ValueError("Vectors must have the same length")
+    
+    sum_squared_distance = 0
+    for i in range(len(x1)):
+        sum_squared_distance += (x1[i] - x2[i])**2
+    return math.sqrt(sum_squared_distance)
+
+
+x1 = [0.303, 0.870, 0.183, 0.250]
+x2 = [0.673, 0.430, 0.800, 0.900]
+print(euclidean_distance(x1,x2))
